5b8b8d56a0d87ba6f160f65fa8c4353e0dcae8d6
Eugenia <eugeniaelvira07@gmail.com>
2025-11-20 20:24:13 +0700

    WIP: Simpan perubahan lokal sebelum pull

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 //toggle active class
 const navbarNav = document.querySelector(".navbar-nav");
+const menu = document.querySelector("#menu");
 //saat menu diklik,
-document.querySelector("#menu").onclick = () => {
+menu.addEventListener("click", () => {
   navbarNav.classList.toggle("active");
-};
+});
 
 //menghilangkan nav dgn klik di mana pun
-const menu = document.querySelector("#menu");
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
+
+//toggle visible class
+const boxAfter = document.querySelector(".box-after");
+const buttonFeatures = document.getElementById("button-features");
+//saat button features diklik,
+if (buttonFeatures && boxAfter) {
+  buttonFeatures.addEventListener("click", () => {
+    boxAfter.classList.toggle("visible");
+    if (boxAfter.classList.contains("visible")) {
+      buttonFeatures.innerHTML = 'Show Less <i data-feather="chevron-up"></i>';
+    } else {
+      buttonFeatures.innerHTML =
+        'Show More <i data-feather="chevron-down"></i>';
+    }
+    feather.replace();
+  });
+}
